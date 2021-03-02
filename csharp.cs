75aee6678a800e5c60ee5c78c6f4f11c56ff37cd
30047778 <79073983+30047778@users.noreply.github.com>
2021-03-02 16:05:25 +0900

    Create csharp.cs
    Add csharp.cs

---
+++ csharp.cs
@@ -0,0 +1,14 @@
+namespace Sample101
+{
+    class Program
+    {
+        /*
+         *  HelloWorld
+         */
+        static void Main(string[] args)
+        {
+            //  コンソールにHelloWorldと表示して終了
+            Console.WriteLine("HelloWorld.");
+        }
+    }
+}
