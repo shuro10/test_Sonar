c609d8ce7c65d9798c09a13c93a79defd2026528
30047778 <79073983+30047778@users.noreply.github.com>
2021-03-05 18:01:17 +0900

    Update HelloWorld.java
    package

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,27 +1,7 @@
-package hello;
+package org.jlleitschuh.testing;
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        HelloWorld helloWorld = new HelloWorld();
-        System.out.println(helloWorld.sayHello());
-    }
-
-    public String sayHello() {
-        return "Hello world!";
-    }
-
-    public String I_Say_Hello() {
-        return "Hello world!";
-    }
-
-    public String SayHello() {
-        return "Hello world!";
-    }
-
-    public String I_Say_Hello_2() {
-        if (true) {
-            return null;
-        }
-        return null;
-    }
+public class App {
+    public static  void main(String[] args) {
+        System.out.println("Hello World!");
+    } 
 }
